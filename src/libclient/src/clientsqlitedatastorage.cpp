6f924da40127214f14db7033d8c1e4f73c14f30b
TsAah[THoF] <tsaah.thof@gmail.com>
2021-04-26 15:01:37 +0300

    some sql interaction unpgrade

--- src/libclient/src/clientsqlitedatastorage.cpp
+++ src/libclient/src/clientsqlitedatastorage.cpp
@@ -16,122 +16,89 @@ ClientSqliteDataStorage::ClientSqliteDataStorage(const QString& databaseFilename
 
 bool ClientSqliteDataStorage::isStoredDeviceCertificateEmpty() const {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.exec("SELECT `id` FROM `deviceCertificate_table` WHERE `id` = 1 LIMIT 1;");
-    SQLASSERT(ok, "query exec");
+    SQLASSERT(q.exec("SELECT `id` FROM `deviceCertificate_table` WHERE `id` = 1 LIMIT 1;"), "query exec");
     return !q.next();
 }
 
 QByteArray ClientSqliteDataStorage::restoreDeviceCertificate() const {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.exec("SELECT `certificate` FROM `deviceCertificate_table` WHERE `id` = 1 LIMIT 1;");
-    SQLASSERT(ok, "query exec");
-    ok = q.next();
-    if (ok) {
-        return q.value(0).toByteArray();
-    } else {
-        return {};
+    QByteArray result;
+    SQLASSERT(q.exec("SELECT `certificate` FROM `deviceCertificate_table` WHERE `id` = 1 LIMIT 1;"), "query exec");
+    if (q.next()) {
+        result = q.value(0).toByteArray();
     }
+    return result;
 }
 
 void ClientSqliteDataStorage::storeDeviceCertificate(const QByteArray &deviceCertificate) {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.prepare("INSERT OR REPLACE INTO `deviceCertificate_table` (`id`, `certificate`) VALUES (1, :cert);");
-    SQLASSERT(ok, "query preparation");
+    SQLASSERT(q.prepare("INSERT OR REPLACE INTO `deviceCertificate_table` (`id`, `certificate`) VALUES (1, :cert);"), "query preparation");
     q.bindValue(":cert", deviceCertificate);
-    ok = q.exec();
-    SQLASSERT(ok, "query exec");
+    SQLASSERT(q.exec(), "query exec");
 }
 
 void ClientSqliteDataStorage::clearDeviceCertificate() {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.prepare("DELETE FROM `deviceCertificate_table` WHERE `id` = 1;");
-    SQLASSERT(ok, "query preparation");
-    ok = q.exec();
-    SQLASSERT(ok, "query exec");
+    SQLASSERT(q.prepare("DELETE FROM `deviceCertificate_table` WHERE `id` = 1;"), "query preparation");
+    SQLASSERT(q.exec(), "query exec");
 }
 
 bool ClientSqliteDataStorage::isStoredUserCertificateEmpty() const {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.exec("SELECT `id` FROM `userCertificate_table` WHERE `id` = 1 LIMIT 1;");
-    SQLASSERT(ok, "query exec");
+    SQLASSERT(q.exec("SELECT `id` FROM `userCertificate_table` WHERE `id` = 1 LIMIT 1;"), "query exec");
     return !q.next();
 }
 
 QByteArray ClientSqliteDataStorage::restoreUserCertificate() const {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.exec("SELECT `certificate` FROM `userCertificate_table` WHERE `id` = 1 LIMIT 1;");
-    SQLASSERT(ok, "query exec");
-    ok = q.next();
-    if (ok) {
-        return q.value(0).toByteArray();
-    } else {
-        return {};
+    QByteArray result;
+    SQLASSERT(q.exec("SELECT `certificate` FROM `userCertificate_table` WHERE `id` = 1 LIMIT 1;"), "query exec");
+    if (q.next()) {
+        result = q.value(0).toByteArray();
     }
+    return result;
 }
 
 void ClientSqliteDataStorage::storeUserCertificate(const QByteArray &userCertificate) {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.prepare("INSERT OR REPLACE INTO `userCertificate_table` (`id`, `certificate`) VALUES (1, :cert);");
-    SQLASSERT(ok, "query preparation");
+    SQLASSERT(q.prepare("INSERT OR REPLACE INTO `userCertificate_table` (`id`, `certificate`) VALUES (1, :cert);"), "query preparation");
     q.bindValue(":cert", userCertificate);
-    ok = q.exec();
-    SQLASSERT(ok, "query exec");
+    SQLASSERT(q.exec(), "query exec");
 }
 
 void ClientSqliteDataStorage::clearUserCertificate() {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.prepare("DELETE FROM `userCertificate_table` WHERE `id` = 1;");
-    SQLASSERT(ok, "query preparation");
-    ok = q.exec();
-    SQLASSERT(ok, "query exec");
+    SQLASSERT(q.prepare("DELETE FROM `userCertificate_table` WHERE `id` = 1;"), "query preparation");
+    SQLASSERT(q.exec(), "query exec");
 }
 
 bool ClientSqliteDataStorage::isStoredUserIdEmpty() const {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.exec("SELECT `id` FROM `userId_table` WHERE `id` = 1 LIMIT 1;");
-    SQLASSERT(ok, "query exec");
+    SQLASSERT(q.exec("SELECT `id` FROM `userId_table` WHERE `id` = 1 LIMIT 1;"), "query exec");
     return !q.next();
 }
 
 quint64 ClientSqliteDataStorage::restoreUserId() const {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.exec("SELECT `userId` FROM `userId_table` WHERE `id` = 1 LIMIT 1;");
-    SQLASSERT(ok, "query exec");
-    ok = q.next();
-    if (ok) {
-        return q.value(0).toULongLong();
-    } else {
-        return 0;
+    quint64 result = 0;
+    SQLASSERT(q.exec("SELECT `userId` FROM `userId_table` WHERE `id` = 1 LIMIT 1;"), "query exec");
+    if (q.next()) {
+        result = q.value(0).toULongLong();
     }
+    return result;
 }
 
 void ClientSqliteDataStorage::storeUserId(quint64 userId) {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.prepare("INSERT OR REPLACE INTO `userId_table` (`id`, `userId`) VALUES (1, :userId);");
-    SQLASSERT(ok, "query preparation");
+    SQLASSERT(q.prepare("INSERT OR REPLACE INTO `userId_table` (`id`, `userId`) VALUES (1, :userId);"), "query preparation");
     q.bindValue(":userId", userId);
-    ok = q.exec();
-    SQLASSERT(ok, "query exec");
+    SQLASSERT(!q.exec(), "query exec");
 }
 
 void ClientSqliteDataStorage::clearUserId() {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.prepare("DELETE FROM `userId_table` WHERE `id` = 1;");
-    SQLASSERT(ok, "query preparation");
-    ok = q.exec();
-    SQLASSERT(ok, "query exec");
+    SQLASSERT(q.prepare("DELETE FROM `userId_table` WHERE `id` = 1;"), "query preparation");
+    SQLASSERT(q.exec(), "query exec");
 }
 
 QSqlDatabase ClientSqliteDataStorage::db() const {
@@ -156,30 +123,26 @@ void ClientSqliteDataStorage::purge() {
 
 void ClientSqliteDataStorage::createTables() {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.exec(R"(
+    SQLASSERT(q.exec(R"(
         CREATE TABLE IF NOT EXISTS `deviceCertificate_table` (
             `id` INTEGER PRIMARY KEY UNIQUE NOT NULL,
             `certificate` BLOB UNIQUE NOT NULL
         );
-    )");
-    SQLASSERT(ok, "query exec");
+    )"), "query exec");
 
-    ok = q.exec(R"(
+    SQLASSERT(q.exec(R"(
         CREATE TABLE IF NOT EXISTS `userCertificate_table` (
             `id` INTEGER PRIMARY KEY UNIQUE NOT NULL,
             `certificate` BLOB UNIQUE NOT NULL
         );
-    )");
-    SQLASSERT(ok, "query exec");
+    )"), "query exec");
 
-    ok = q.exec(R"(
+    SQLASSERT(q.exec(R"(
         CREATE TABLE IF NOT EXISTS `userId_table` (
             `id` INTEGER PRIMARY KEY UNIQUE NOT NULL,
             `userId` INT UNIQUE NOT NULL
         );
-    )");
-    SQLASSERT(ok, "query exec");
+    )"), "query exec");
 }
 
 
--- src/libserver/src/serversqlitedatastorage.cpp
+++ src/libserver/src/serversqlitedatastorage.cpp
@@ -18,129 +18,97 @@ ServerSqliteDataStorage::ServerSqliteDataStorage(QObject *parent): IServerDataSt
 bool ServerSqliteDataStorage::deviceExists(const QByteArray &deviceCert) const {
     const auto hash = qHash(deviceCert);
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.prepare("SELECT `hash` FROM `device_table` WHERE `hash` = :hash LIMIT 1;");
-    SQLASSERT(ok, "query preparation");
+    bool result = false;
+    SQLASSERT(q.prepare("SELECT `hash` FROM `device_table` WHERE `hash` = :hash LIMIT 1;"), "query preparation");
     q.bindValue(":hash", hash);
-    ok = q.exec();
-    SQLASSERT(ok, "query exec");
+    SQLASSERT(q.exec(), "query exec");
     if (q.next()) {
         const auto dbHash = q.value("hash").toUInt();
         wDebug << dbHash << hash << deviceCert.left(10);
-        return dbHash == hash;
+        result = dbHash == hash;
     }
-    return false;
+    return result;
 }
 
 void ServerSqliteDataStorage::rememberDevice(const QByteArray &deviceCert) {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.prepare("INSERT INTO `device_table` (`hash`, `certificate`) VALUES (:hash, :cert);");
-    SQLASSERT(ok, "query preparation");
+    SQLASSERT(q.prepare("INSERT INTO `device_table` (`hash`, `certificate`) VALUES (:hash, :cert);"), "query preparation");
     q.bindValue(":hash", qHash(deviceCert));
     q.bindValue(":cert", deviceCert);
-    ok = q.exec();
-    SQLASSERT(ok, "query exec");
+    SQLASSERT(q.exec(), "query exec");
 }
 
 bool ServerSqliteDataStorage::userExists(const QByteArray &userCert) const {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.prepare("SELECT `id` FROM `user_table` WHERE `hash` = :hash LIMIT 1;");
-    SQLASSERT(ok, "query preparation");
+    SQLASSERT(q.prepare("SELECT `id` FROM `user_table` WHERE `hash` = :hash LIMIT 1;"), "query preparation");
     q.bindValue(":hash", qHash(userCert));
-    ok = q.exec();
-    SQLASSERT(ok, "query exec");
-    if (q.next()) {
-        return true;
-    }
-    return false;
+    SQLASSERT(q.exec(), "query exec");
+    return q.next();
 }
 
 bool ServerSqliteDataStorage::userIdExists(quint64 userId) const {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.prepare("SELECT `id` FROM `user_table` WHERE `id` = :id LIMIT 1;");
-    SQLASSERT(ok, "query preparation");
+    bool result = false;
+    SQLASSERT (q.prepare("SELECT `id` FROM `user_table` WHERE `id` = :id LIMIT 1;"), "query preparation");
     q.bindValue(":id", userId);
-    ok = q.exec();
-    SQLASSERT(ok, "query exec");
+    SQLASSERT(q.exec(), "query exec");
     if (q.next()) {
         const auto dbUserId = q.value("id").toUInt();
-        return dbUserId == userId;
+        result = dbUserId == userId;
     }
-    return false;
+    return result;
 }
 
 void ServerSqliteDataStorage::rememberNewUser(quint64 userId, const QByteArray &userCert, const QByteArray &deviceCert, const QByteArray &passwordHash, const QByteArray& passwordSalt) {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.prepare("INSERT INTO `user_table` (`hash`, `id`, `certificate`, `passwordHash`, `passwordSalt`) VALUES (:hash, :id, :cert, :passwordHash, :passwordSalt);");
-    SQLASSERT(ok, "query preparation");
+    SQLASSERT(q.prepare("INSERT INTO `user_table` (`hash`, `id`, `certificate`, `passwordHash`, `passwordSalt`) VALUES (:hash, :id, :cert, :passwordHash, :passwordSalt);"), "query preparation");
     q.bindValue(":hash", qHash(userCert));
     q.bindValue(":id", userId);
     q.bindValue(":cert", userCert);
     q.bindValue(":passwordHash", passwordHash);
     q.bindValue(":passwordSalt", passwordSalt);
-    ok = q.exec();
-    SQLASSERT(ok, "query exec");
+    SQLASSERT(q.exec(), "query exec");
     q.finish();
-
-    ok = q.prepare("INSERT INTO `user_devices_table` (`userHash`, `deviceHash`) VALUES (:userHash, :deviceHash);");
-    SQLASSERT(ok, "query preparation");
+    SQLASSERT(q.prepare("INSERT INTO `user_devices_table` (`userHash`, `deviceHash`) VALUES (:userHash, :deviceHash);"), "query preparation");
     q.bindValue(":userHash", qHash(userCert));
     q.bindValue(":deviceHash", qHash(deviceCert));
-    ok = q.exec();
-    SQLASSERT(ok, "query exec");
-
+    SQLASSERT(q.exec(), "query exec");
 }
 
 QByteArray ServerSqliteDataStorage::getUserCertificate(quint64 userId) const {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.prepare("SELECT `certificate` FROM `user_table` WHERE `id` = :id;");
-    SQLASSERT(ok, "query preparation");
+    QByteArray result;
+    SQLASSERT(q.prepare("SELECT `certificate` FROM `user_table` WHERE `id` = :id;"), "query preparation");
     q.bindValue(":id", userId);
-    ok = q.exec();
-    SQLASSERT(ok, "query exec");
-    ok = q.next();
-    if (ok) {
-        return q.value("certificate").toByteArray();
-    } else {
-        return {};
+    SQLASSERT(q.exec(), "query exec");
+    if (q.next()) {
+        result = q.value("certificate").toByteArray();
     }
+    return result;
 }
 
 QByteArray ServerSqliteDataStorage::getPasswordHash(quint64 userId) const {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.prepare("SELECT `passwordHash` FROM `user_table` WHERE `id` = :id;");
-    SQLASSERT(ok, "query preparation");
+    QByteArray result;
+    SQLASSERT(q.prepare("SELECT `passwordHash` FROM `user_table` WHERE `id` = :id;"), "query preparation");
     q.bindValue(":id", userId);
-    ok = q.exec();
-    SQLASSERT(ok, "query exec");
-    ok = q.next();
-    if (ok) {
-        return q.value("passwordHash").toByteArray();
-    } else {
-        return {};
+    SQLASSERT(q.exec(), "query exec");
+    if (q.next()) {
+        result = q.value("passwordHash").toByteArray();
     }
+    return result;
 }
 
 QByteArray ServerSqliteDataStorage::getPasswordSalt(quint64 userId) const {
     QSqlQuery q(db());
-    bool ok = false;
-    ok = q.prepare("SELECT `passwordSalt` FROM `user_table` WHERE `id` = :id;");
-    SQLASSERT(ok, "query preparation");
+    QByteArray result;
+    SQLASSERT(q.prepare("SELECT `passwordSalt` FROM `user_table` WHERE `id` = :id;"), "query preparation");
     q.bindValue(":id", userId);
-    ok = q.exec();
-    SQLASSERT(ok, "query exec");
-    ok = q.next();
-    if (ok) {
-        return q.value("passwordSalt").toByteArray();
-    } else {
-        return {};
+    SQLASSERT(q.exec(), "query exec");
+    if (q.next()) {
+        result = q.value("passwordSalt").toByteArray();
     }
+    return result;
 }
 
 QSqlDatabase ServerSqliteDataStorage::db() const {
@@ -166,30 +134,30 @@ void ServerSqliteDataStorage::purge() {
 
 void ServerSqliteDataStorage::createTables() {
     QSqlQuery q(db());
-    bool ok = q.exec(R"(
-            CREATE TABLE IF NOT EXISTS `device_table` (
-                `hash` INTEGER PRIMARY KEY UNIQUE NOT NULL,
-                `certificate` BLOB UNIQUE NOT NULL
-            );
-        )");
-    SQLASSERT(ok, "query exec");
-    ok = q.exec(R"(
-            CREATE TABLE IF NOT EXISTS `user_table` (
-                `hash` INTEGER PRIMARY KEY UNIQUE NOT NULL,
-                `id` INTEGER UNIQUE NOT NULL,
-                `certificate` BLOB UNIQUE NOT NULL,
-                `passwordHash` BLOB NOT NULL,
-                `passwordSalt` BLOB NOT NULL
-            );
-        )");
-    SQLASSERT(ok, "query exec");
-    ok = q.exec(R"(
-            CREATE TABLE IF NOT EXISTS `user_devices_table` (
-                `userHash` INT NOT NULL,
-                `deviceHash` INT NOT NULL
-            );
-        )");
-    SQLASSERT(ok, "query exec");
+
+    SQLASSERT(q.exec(R"(
+        CREATE TABLE IF NOT EXISTS `device_table` (
+            `hash` INTEGER PRIMARY KEY UNIQUE NOT NULL,
+            `certificate` BLOB UNIQUE NOT NULL
+        );
+    )"), "query exec");
+
+    SQLASSERT(q.exec(R"(
+        CREATE TABLE IF NOT EXISTS `user_table` (
+            `hash` INTEGER PRIMARY KEY UNIQUE NOT NULL,
+            `id` INTEGER UNIQUE NOT NULL,
+            `certificate` BLOB UNIQUE NOT NULL,
+            `passwordHash` BLOB NOT NULL,
+            `passwordSalt` BLOB NOT NULL
+        );
+    )"), "query exec");
+
+    SQLASSERT(q.exec(R"(
+        CREATE TABLE IF NOT EXISTS `user_devices_table` (
+            `userHash` INT NOT NULL,
+            `deviceHash` INT NOT NULL
+        );
+    )"), "query exec");
 }
 
 } // namespace server
